5c482af92b90ee346d83ae78e882e9225128a9a2
Frank Song <franksongfeng@users.noreply.github.com>
2019-12-12 11:04:31 +0800

    Update startup.sh
    make ssh work with a fixed password

--- startup.sh
+++ startup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # prepare ssh server
-mkdir -p /var/run/sshd
+ROOT_PWD=myadmin
+echo 'root:'${ROOT_PWD} | chpasswd
+mkdir -p /var/run/sshd && \
+    sed -ri 's/^PermitRootLogin\s+.*/PermitRootLogin yes/' /etc/ssh/sshd_config && \
+    sed -ri "s/^Port\s+.*/Port 22/" /etc/ssh/sshd_config && \
+    sed -ri 's/UsePAM yes/#UsePAM yes/g' /etc/ssh/sshd_config
 
 # start up supervisord, all daemons should launched by supervisord.
 /usr/bin/supervisord -c /root/supervisord.conf
